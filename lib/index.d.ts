0b0ed275f0f32e943c054d6734fc49c26810b709
Justin <calleja.justin@gmail.com>
2016-06-18 20:46:31 +0200

    Removed lib and added .gitignore

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Node {
-    label: string;
-    nodes: Node[];
-}
-export interface ChildrenOfFun {
-    (parent: string, data: any): string[];
-}
-export default function archify(label: string, data: any, childrenOf: ChildrenOfFun): any;
