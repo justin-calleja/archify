ff32969a144d888279f7dcb6ec4712bcb30c1f7e
Justin <calleja.justin@gmail.com>
2016-06-18 20:47:47 +0200

    Removed test/*

--- test/index.ts
+++
@@ -1,8 +0,0 @@
-// var assert = require('chai').assert;
-//
-// suite('pkg-dependents interface', function() {
-//   test('expect undefined value for abc123 package since it does not exist in given paths', function (cb) {
-//   });
-// });
-
-console.log('hello world');
