156194aaf777e0013fda0f870ef58f0b5bd7adcf
Justin <calleja.justin@gmail.com>
2016-06-19 02:37:40 +0200

    Added test and added ability to add suffix/prefix to label in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,35 @@ export interface Node {
   nodes: Node[];
 }
 
-export interface ChildrenOfFun {
-  (parent: string, data: any): string[];
+export interface LabelParts {
+  label: string,
+  prefix?: string,
+  suffix?: string
 }
 
-export default function archify(label: string, data: any, childrenOf: ChildrenOfFun) {
-  var kids: string[] = childrenOf(label, data);
+export interface ChildrenOfFun {
+  (parent: string, data: any): LabelParts[];
+}
+
+export interface Opts {
+  labelPrefix?: string,
+  labelSuffix?: string
+}
+
+// Note: if you're going to make default, remember to access with .default from commonjs modules
+// https://github.com/Microsoft/TypeScript/issues/2719
+export function archify(label: string, data: any, childrenOf: ChildrenOfFun, opts: Opts = {}) {
+  var kids: LabelParts[] = childrenOf(label, data);
+  var nodes = kids.map(kid => {
+    var opts = {
+      labelPrefix: kid.prefix,
+      labelSuffix: kid.suffix
+    };
+    return archify(kid.label, data, childrenOf, opts);
+  });
+
   return {
-    label,
-    nodes: kids.map(kid => archify(kid, data, childrenOf))
+    label: (opts.labelPrefix || '') + label + (opts.labelSuffix || ''),
+    nodes
   }
 }
---
+++ test/index.js
@@ -0,0 +1,40 @@
+var assert = require('chai').assert;
+var archify = require('../lib').archify;
+var data = require('./fixtures/abc');
+
+function childrenOf(parent, data) {
+  var dependents = data[parent].dependents;
+  var depsLP = dependents.dependencyDependents.map(dep => ({ label: dep.pkgJSON.name, suffix: `.dependencies['${parent}']` }));
+  var peerDepsLP = dependents.peerDependencyDependents.map(peerDep => ({
+    prefix: '[peer] ',
+    label: peerDep.pkgJSON.name,
+    suffix: `.peerDependencies['${parent}']`
+  }));
+  var devDepsLP = dependents.devDependencyDependents.map(devDep => ({
+    prefix: '[dev] ',
+    label: devDep.pkgJSON.name,
+    suffix: `.devDependencies['${parent}']`
+  }));
+
+  return depsLP.concat(peerDepsLP).concat(devDepsLP);
+}
+
+suite('#archify()', function() {
+  test('when root is d', function () {
+    var res = archify('d', data, childrenOf);
+    assert.equal(res.label, 'd', 'Root should have label of "d"');
+
+    var aNode = res.nodes.find(node => node.label.startsWith('[peer] a'));
+    assert.isFalse(aNode === undefined);
+    assert.equal(aNode.label, '[peer] a.peerDependencies[\'d\']');
+    assert.equal(aNode.nodes.length, 0, 'The depedent a has no dependents of its own')
+
+    var bNode = res.nodes.find(node => node.label.startsWith('[dev] b'));
+    assert.equal(bNode.label, '[dev] b.devDependencies[\'d\']');
+    assert.equal(bNode.nodes.length, 1, 'The depedent b has one dependent of its own')
+
+    var bNodeANode = bNode.nodes[0];
+    assert.isFalse(bNodeANode === undefined);
+    assert.equal(bNodeANode.label, 'a.dependencies[\'b\']');
+  });
+});
